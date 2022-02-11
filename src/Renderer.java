582313ca5c0fe1763ff47fb3390abb13e388fe0a
Lex Stapleton <diamondcrafter1423@gmail.com>
2022-02-11 11:06:12 -0500

    Added project
    Added project template to repo

---
+++ src/Renderer.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLProfile;
+
+public class Renderer
+{
+private static GLWindow window;
+
+    public static void init(){
+        GLProfile.initSingleton();
+        GLProfile profile = GLProfile.get(GLProfile.GL4);
+        GLCapabilities caps = new GLCapabilities(profile);
+
+        window = GLWindow.create(caps);
+        window.setSize(640, 360);
+    }    
+}
