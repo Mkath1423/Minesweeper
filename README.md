a7203c38e221b7cd254c6814e2ab1c8784aa9d93
Mkath1423 <diamondcrafter1423@gmail.com>
2022-02-13 22:20:37 -0500

    Added Tutorials

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Minesweeper
 Minesweeper built in Java OpenGL
+
+Tutorials
+- https://github.com/Jojal/modern-jogl-examples/blob/master/modern-jogl-examples/src/test/Test.java
+- https://www.javatpoint.com/jogl-classes
--- src/Renderer.java
+++ src/Renderer.java
@@ -4,16 +4,27 @@ import com.jogamp.newt.opengl.GLWindow;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLProfile;
 
+import javax.media.opengl.awt.GLCanavas
+
 public class Renderer
 {
-private static GLWindow window;
+    private static GLWindow window;
 
-    public static void init(){
-        GLProfile.initSingleton();
-        GLProfile profile = GLProfile.get(GLProfile.GL4);
-        GLCapabilities caps = new GLCapabilities(profile);
+        public static void init(){
+            GLProfile.initSingleton();
+            GLProfile profile = GLProfile.get(GLProfile.GL4);
+            GLCapabilities caps = new GLCapabilities(profile);
+            
+      
 
-        window = GLWindow.create(caps);
-        window.setSize(640, 360);
-    }    
+            GlCanvas canvas = new GLCanavas(caps);
+            Frame frame = new Frame();
+            frame.add(canvas);
+            
+        }    
+
+        public static void main(String[] args){
+            System.out.println("main");
+            init();
+        }
 }
