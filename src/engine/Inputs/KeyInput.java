1a77abfc7257b01581532a74cec793dca9c2025f
Mkath1423 <diamondcrafter1423@gmail.com>
2022-03-07 11:02:11 -0500

    Added KeyInputs code
    Added key ocdes to the KeyInputs Enum

--- src/engine/Inputs/KeyInput.java
+++ src/engine/Inputs/KeyInput.java
@@ -1,18 +1,42 @@
 package engine.Inputs;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
 import com.jogamp.newt.event.KeyEvent;
-import com.jogamp.newt.event.KeyListener;
+import com.jogamp.newt.event.MouseEvent;
 
-public class KeyInput implements KeyListener{
+public class KeyInput{
+    private static Map<KeyInputKeys, List<Consumer<KeyEvent>>> listeners;
 
-    @Override
-    public void keyPressed(KeyEvent data) {
-        
+    static {
+        listeners = new EnumMap<KeyInputKeys, List<Consumer<KeyEvent>>>(KeyInputKeys.class);
+
+        for (KeyInputKeys mouseInput : KeyInputKeys.values()) {
+            listeners.put(mouseInput, new ArrayList<Consumer<KeyEvent>>());
+        }
     }
 
-    @Override
-    public void keyReleased(KeyEvent data) {
-        
+    public static void raiseListeners(KeyInputKeys key, KeyEvent data){
+        for(Consumer<KeyEvent> consumer : listeners.get(key)){
+            consumer.accept(data);
+        }
     }
-    
+
+    public static int register(KeyInputKeys key, Consumer<KeyEvent> consumer){
+        listeners.get(key).add(consumer);
+        return listeners.get(key).size() - 1;
+    }
+
+    public static boolean unregister(KeyInputKeys key, int index){
+        if(index > listeners.get(key).size() -1) return false;
+
+        listeners.get(key).remove(index);
+
+        return true;
+    }
+
 }
--- src/engine/Inputs/KeyInputCaller.java
+++ src/engine/Inputs/KeyInputCaller.java
@@ -1,5 +1,19 @@
 package engine.Inputs;
 
-public class KeyInputCaller {
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+
+public class KeyInputCaller implements KeyListener{
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        KeyInput.raiseListeners(KeyInputKeys.get(event.getKeyCode()), event);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
